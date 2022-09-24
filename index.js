3da079779dae244c8886e1208de1cfc9fd7d7bb1
ardiantohendy <93106692+ardiantohendy@users.noreply.github.com>
2022-09-24 20:54:44 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const firstPage = document.querySelector(".wrapFirstPage");
+const aboutPage = document.querySelector(".wrapAboutPage");
+const listOfOptions = document.querySelectorAll(".headerMenu li a");
+const footerOfOptions = document.querySelectorAll(".headerMenu li a");
+
+// for (let i = 0; i < listOfOptions.length; i++) {
+//   listOfOptions[i].addEventListener("click", function () {
+//     if (listOfOptions[i] === 0) {
+//       //   aboutPage.style.display = "none";
+//       alert("Portfolio masih dalam pengembangan ya");
+//     } else if (listOfOptions[i] === 1) {
+//       alert("Portfolio masih dalam pengembangan ya");
+//     } else if (listOfOptions[i] === 2) {
+//       alert("Portfolio masih dalam pengembangan ya");
+//       //   firstPage.style.display = "none";
+//       //   aboutPage.style.display = "block";
+//     }
+//   });
+// }
+
+// listOfOptions[0].addEventListener("click", function () {
+//   if (firstPage.style.display == "none") firstPage.style.display = "block";
+//   aboutPage.style.display = "none";
+//   firstPage.style.display = "block";
+//   //   alert("Portfolio masih dalam pengembangan ya");
+// });
+
+// listOfOptions[2].addEventListener("click", function () {
+//   aboutPage.style.display = "block";
+//   firstPage.style.display = "none";
+//   //   alert("Portfolio masih dalam pengembangan ya");
+// });
